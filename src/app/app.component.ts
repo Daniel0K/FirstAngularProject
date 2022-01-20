e6b217cfb938f91ae8a2c865e72e7643c7ccff17
Daniel Kotov <sasukesharingan@mail.ru>
2022-01-20 19:55:48 +0700

    Пытаюсь заставить работать husky v7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,7 +34,6 @@ export class AppComponent {
       );
       for (let i = 0; i < this.cookiesTest.length; i++) {
         this.TestActionsService.houses.push(this.cookiesTest[i]);
-
       }
     }
   }
