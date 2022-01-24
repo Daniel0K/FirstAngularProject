372beb982237cc6765fc4ddea5d0f19f23b90b77
Daniel Kotov <sasukesharingan@mail.ru>
2022-01-25 00:12:16 +0700

    Покрасил выбранный pin в синий цвет v2

--- src/app/pins/pins.component.ts
+++ src/app/pins/pins.component.ts
@@ -1,18 +1,26 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Pin } from '../models/pin'
+import { Pin } from '../models/pin';
+import { PinsService } from '../services/pins.service';
+
 @Component({
   selector: 'app-pins',
   templateUrl: './pins.component.html',
   styleUrls: ['./pins.component.scss'],
 })
 export class PinsComponent implements OnInit {
-  @Input() inputPin!: Pin;
+  constructor(private pinService: PinsService) {}
 
+  @Input() inputPin!: Pin;
   styleCord: string = '';
   styleActivePinColor: string = '';
+  activePin: Pin = {} as Pin;
 
   ngOnInit() {
     this.changeStylePosition(this.inputPin.x, this.inputPin.y);
+  }
+
+  ngDoCheck() {
+    this.activePin = this.pinService.activePin;
   }
 
   changeStylePosition(x: number, y: number): void {
